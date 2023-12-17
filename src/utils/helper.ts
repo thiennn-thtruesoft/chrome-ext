9c5d84771dc684ea6dc51645f03a28de52ffa1e4
thienmta <thienmta.py@gmail.com>
2023-12-17 20:23:34 +0700

    Initial Project

---
+++ src/utils/helper.ts
@@ -0,0 +1,26 @@
+export function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export const mouseDownEvent = new MouseEvent('mousedown', {
+    bubbles: true,
+    cancelable: true,
+    clientX: 20,
+    clientY: 20
+})
+
+export const mouseUpEvent = new MouseEvent('mouseup', {
+    bubbles: true,
+    cancelable: true,
+    clientX: 20,
+    clientY: 20
+})
+
+export async function talk(something: string) {
+    const el = document.querySelector('.askt-utterance__record')
+    if (!el) return alert('no element found')
+    el.dispatchEvent(mouseDownEvent)
+    window.speechSynthesis.speak(new SpeechSynthesisUtterance(something))
+    await sleep(5000)
+    el.dispatchEvent(mouseUpEvent)
+}
